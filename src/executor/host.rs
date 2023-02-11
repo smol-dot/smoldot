cdc66ce831a3bc03ce8b0885ca7027cdceafa272
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-02-11 15:12:32 +0100

    Remove the ext_sandbox_* host functions

--- src/executor/host.rs
+++ src/executor/host.rs
@@ -1532,14 +1532,6 @@ impl ReadyToRun {
             HostFunction::ext_offchain_http_response_read_body_version_1 => {
                 host_fn_not_implemented!()
             }
-            HostFunction::ext_sandbox_instantiate_version_1 => host_fn_not_implemented!(),
-            HostFunction::ext_sandbox_invoke_version_1 => host_fn_not_implemented!(),
-            HostFunction::ext_sandbox_memory_new_version_1 => host_fn_not_implemented!(),
-            HostFunction::ext_sandbox_memory_get_version_1 => host_fn_not_implemented!(),
-            HostFunction::ext_sandbox_memory_set_version_1 => host_fn_not_implemented!(),
-            HostFunction::ext_sandbox_memory_teardown_version_1 => host_fn_not_implemented!(),
-            HostFunction::ext_sandbox_instance_teardown_version_1 => host_fn_not_implemented!(),
-            HostFunction::ext_sandbox_get_global_val_version_1 => host_fn_not_implemented!(),
             HostFunction::ext_trie_blake2_256_root_version_1
             | HostFunction::ext_trie_blake2_256_root_version_2 => {
                 let state_version =
@@ -3217,14 +3209,6 @@ externalities! {
     ext_offchain_http_response_wait_version_1,
     ext_offchain_http_response_headers_version_1,
     ext_offchain_http_response_read_body_version_1,
-    ext_sandbox_instantiate_version_1,
-    ext_sandbox_invoke_version_1,
-    ext_sandbox_memory_new_version_1,
-    ext_sandbox_memory_get_version_1,
-    ext_sandbox_memory_set_version_1,
-    ext_sandbox_memory_teardown_version_1,
-    ext_sandbox_instance_teardown_version_1,
-    ext_sandbox_get_global_val_version_1,
     ext_trie_blake2_256_root_version_1,
     ext_trie_blake2_256_root_version_2,
     ext_trie_blake2_256_ordered_root_version_1,
@@ -3327,14 +3311,6 @@ impl HostFunction {
             HostFunction::ext_offchain_http_response_wait_version_1 => todo!(),
             HostFunction::ext_offchain_http_response_headers_version_1 => todo!(),
             HostFunction::ext_offchain_http_response_read_body_version_1 => todo!(),
-            HostFunction::ext_sandbox_instantiate_version_1 => todo!(),
-            HostFunction::ext_sandbox_invoke_version_1 => todo!(),
-            HostFunction::ext_sandbox_memory_new_version_1 => todo!(),
-            HostFunction::ext_sandbox_memory_get_version_1 => todo!(),
-            HostFunction::ext_sandbox_memory_set_version_1 => todo!(),
-            HostFunction::ext_sandbox_memory_teardown_version_1 => todo!(),
-            HostFunction::ext_sandbox_instance_teardown_version_1 => todo!(),
-            HostFunction::ext_sandbox_get_global_val_version_1 => todo!(),
             HostFunction::ext_trie_blake2_256_root_version_1 => 1,
             HostFunction::ext_trie_blake2_256_root_version_2 => 2,
             HostFunction::ext_trie_blake2_256_ordered_root_version_1 => 1,
