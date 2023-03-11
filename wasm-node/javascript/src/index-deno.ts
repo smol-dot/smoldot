e942afdd743b5fef32992f7d914bd9318e7fb9f7
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-03-11 11:13:52 +0100

    ! instead of ?

--- wasm-node/javascript/src/index-deno.ts
+++ wasm-node/javascript/src/index-deno.ts
@@ -228,7 +228,7 @@ function connect(config: ConnectionConfig, forbidTcp: boolean, forbidWs: boolean
             } else {
                 // TCP
                 connection.socket.destroyed = true;
-                connection.socket.inner.then((connec) => connec?.close());
+                connection.socket.inner.then((connec) => connec!.close());
             }
         },
 
