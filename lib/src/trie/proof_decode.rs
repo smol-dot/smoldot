ed2fa223d5a4a82ec991bf690ee0cbdad428151c
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-03-28 19:58:09 +0200

    Rustfmt

--- lib/src/trie/proof_decode.rs
+++ lib/src/trie/proof_decode.rs
@@ -799,7 +799,9 @@ impl<'a> Children<'a> {
     }
 
     /// Returns an iterator of 16 items, one for each child.
-    pub fn children(&'_ self) -> impl DoubleEndedIterator + ExactSizeIterator<Item = Child<'a>> + '_ {
+    pub fn children(
+        &'_ self,
+    ) -> impl DoubleEndedIterator + ExactSizeIterator<Item = Child<'a>> + '_ {
         self.children.iter().copied()
     }
 }
