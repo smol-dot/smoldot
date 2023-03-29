0475608187ae2f1071afd3c46a60e6f44bf02d65
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-03-29 11:37:49 +0000

    Remove the `recursion_limit` attributes (#371)

--- light-base/src/lib.rs
+++ light-base/src/lib.rs
@@ -65,7 +65,6 @@
 // TODO: talk about the fact that a randomness environment is assumed?
 
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
-#![recursion_limit = "512"]
 #![deny(rustdoc::broken_intra_doc_links)]
 // TODO: the `unused_crate_dependencies` lint is disabled because of dev-dependencies, see <https://github.com/rust-lang/rust/issues/95513>
 // #![deny(unused_crate_dependencies)]
