fd10b7df8f3c1bfbcb0795e4a01515ecfdc86e12
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-03-29 13:21:00 +0200

    Remove the `recursion_limit` attributes

--- full-node/src/main.rs
+++ full-node/src/main.rs
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![recursion_limit = "1024"]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(unused_crate_dependencies)]
 
