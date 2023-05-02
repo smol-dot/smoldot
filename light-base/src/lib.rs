e9b07c39e2452a7689746b2c4680015e126c055f
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-05-02 15:32:31 +0200

    Revert randomness changes

--- light-base/src/lib.rs
+++ light-base/src/lib.rs
@@ -647,7 +647,7 @@ impl<TPlat: platform::PlatformRef, TChain> Client<TPlat, TChain> {
             Entry::Vacant(entry) => {
                 // Key used by the networking. Represents the identity of the node on the
                 // peer-to-peer network.
-                let network_noise_key = connection::NoiseKey::new(&[0; 32]); // TODO: temporary hack due to no TLS for wasi+threads at the moment
+                let network_noise_key = connection::NoiseKey::new(&rand::random());
 
                 // Version of the client when requested through the networking.
                 let network_identify_agent_version = format!(
