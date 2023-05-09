dafe9c74f243628c474bc9247edc7fc7448daf30
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-05-09 08:23:02 +0200

    Use a Vec<u8> instead of Header in ChainInformationBuild

--- lib/src/sync/warp_sync.rs
+++ lib/src/sync/warp_sync.rs
@@ -1297,10 +1297,11 @@ impl<TSrc, TRq> BuildRuntime<TSrc, TRq> {
                         }
                     } else {
                         chain_information::build::ConfigFinalizedBlockHeader::NonGenesis {
-                            header: header.clone(),
+                            header: header.scale_encoding_vec(self.inner.block_number_bytes),
                             known_finality: Some(chain_information_finality.clone()),
                         }
                     },
+                    block_number_bytes: self.inner.block_number_bytes,
                     runtime,
                 },
             );
