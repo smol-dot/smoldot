7a9a3b38678c4f2bb84d5f6c4d26ee50bbdd6d48
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-06-01 11:35:16 +0200

    Add failing fuzzing test

--- lib/src/executor/trie_root_calculator/tests.rs
+++ lib/src/executor/trie_root_calculator/tests.rs
@@ -19,12 +19,9 @@
 
 use super::{trie_root_calculator, Config, InProgress, TrieEntryVersion};
 use crate::{executor::storage_diff::TrieDiff, trie};
-use core::iter;
+use core::{iter, ops};
 
-use rand::{
-    distributions::{Distribution as _, Uniform},
-    seq::SliceRandom as _,
-};
+use rand::distributions::{Distribution as _, Uniform};
 
 #[test]
 fn empty_trie_works() {
@@ -189,8 +186,7 @@ fn new_branch_node() {
     }
 }
 
-// TODO: finish
-/*#[test]
+#[test]
 fn fuzzing() {
     fn uniform_sample(min: u8, max: u8) -> u8 {
         Uniform::new_inclusive(min, max).sample(&mut rand::thread_rng())
@@ -199,25 +195,43 @@ fn fuzzing() {
     // We run the test multiple times because of randomness.
     for _ in 0..256 {
         // Create a random trie.
-        let mut trie_before_diff = trie::trie_structure::TrieStructure::new();
-        for _ in 0..5 {
-            let mut list = vec![Vec::new()];
-            for elem in list.clone().into_iter() {
-                for _ in 0..uniform_sample(0, 4) {
-                    let mut elem = elem.clone();
-                    for _ in 0..uniform_sample(0, 3) {
-                        elem.push(trie::Nibble::try_from(uniform_sample(0, 15)).unwrap());
-                    }
-                    list.push(elem);
+        // Each node contains a `Some` with the storage value or `None` for branch nodes, plus its
+        // Merkle value as `Some` if already calculated.
+        let mut trie_before_diff = trie::trie_structure::TrieStructure::<(
+            Option<Vec<u8>>,
+            Option<trie::trie_node::MerkleValueOutput>,
+        )>::new();
+
+        let mut list = vec![Vec::new()];
+        for elem in list.clone().into_iter() {
+            for _ in 0..uniform_sample(0, 4) {
+                let mut elem = elem.clone();
+                for _ in 0..uniform_sample(1, 3) {
+                    elem.push(uniform_sample(0, 255));
                 }
+                list.push(elem);
             }
-            for elem in list {
-                trie_before_diff
-                    .node(elem.into_iter())
-                    .into_vacant()
-                    .unwrap()
-                    .insert_storage_value()
-                    .insert(None, None);
+        }
+        for elem in list {
+            let mut storage_value = Vec::new();
+            for _ in 0..uniform_sample(0, 3) {
+                storage_value.push(uniform_sample(0, 255));
+            }
+
+            match trie_before_diff.node(trie::bytes_to_nibbles(elem.iter().copied())) {
+                trie::trie_structure::Entry::Vacant(e) => {
+                    e.insert_storage_value()
+                        .insert((Some(storage_value), None), (None, None));
+                }
+                trie::trie_structure::Entry::Occupied(
+                    trie::trie_structure::NodeAccess::Branch(mut e),
+                ) => {
+                    *e.user_data() = (Some(storage_value), None);
+                    e.insert_storage_value();
+                }
+                trie::trie_structure::Entry::Occupied(
+                    trie::trie_structure::NodeAccess::Storage(_),
+                ) => unreachable!(),
             }
         }
 
@@ -226,143 +240,145 @@ fn fuzzing() {
         let mut trie_after_diff = trie_before_diff.clone();
         let mut diff = TrieDiff::empty();
 
-        // Generate a set of keys that will find themselves in the tries in the end.
-        let final_storage: HashSet<Vec<Nibble>> = {
+        for _ in 0..5 {
             let mut list = vec![Vec::new()];
-            for _ in 0..5 {
-                for elem in list.clone().into_iter() {
-                    for _ in 0..uniform_sample(0, 4) {
-                        let mut elem = elem.clone();
-                        for _ in 0..uniform_sample(0, 3) {
-                            elem.push(Nibble::try_from(uniform_sample(0, 15)).unwrap());
-                        }
-                        list.push(elem);
+            for elem in list.clone().into_iter() {
+                for _ in 0..uniform_sample(0, 4) {
+                    let mut elem = elem.clone();
+                    for _ in 0..uniform_sample(0, 3) {
+                        elem.push(uniform_sample(0, 255));
+                    }
+                    list.push(elem);
+                }
+            }
+            for elem in list {
+                let mut storage_value = Vec::new();
+                for _ in 0..uniform_sample(0, 3) {
+                    storage_value.push(uniform_sample(0, 255));
+                }
+
+                match trie_after_diff.node(trie::bytes_to_nibbles(elem.iter().copied())) {
+                    trie::trie_structure::Entry::Occupied(
+                        trie::trie_structure::NodeAccess::Storage(e),
+                    ) => {
+                        e.remove();
+                        diff.diff_insert_erase(elem, ());
+                    }
+                    trie::trie_structure::Entry::Occupied(
+                        trie::trie_structure::NodeAccess::Branch(mut e),
+                    ) => {
+                        *e.user_data() = (Some(storage_value.clone()), None);
+                        e.insert_storage_value();
+                        diff.diff_insert(elem, storage_value, ());
+                    }
+                    trie::trie_structure::Entry::Vacant(e) => {
+                        e.insert_storage_value()
+                            .insert((Some(storage_value.clone()), None), (None, None));
+                        diff.diff_insert(elem, storage_value, ());
                     }
                 }
             }
-            list.into_iter().skip(1).collect()
+        }
+
+        // Use the trie_root_calculator to calculate the root of `trie_after_diff`.
+        let obtained_hash = {
+            let mut calculator = trie_root_calculator(Config {
+                diff: diff.clone(),
+                diff_trie_entries_version: TrieEntryVersion::V0, // TODO: test multiple trie versions?
+                max_trie_recalculation_depth_hint: 8,
+            });
+
+            loop {
+                match calculator {
+                    InProgress::Finished { trie_root_hash } => break trie_root_hash,
+                    InProgress::ClosestDescendant(req) => {
+                        let mut next_node = trie_before_diff
+                            .range_iter(
+                                ops::Bound::Included(req.key_as_vec().into_iter()),
+                                ops::Bound::Unbounded::<iter::Empty<trie::Nibble>>,
+                            )
+                            .next();
+                        // Set `next_node` to `None` if it isn't a descendant of the demanded key.
+                        if next_node.map_or(false, |n| {
+                            !trie_before_diff
+                                .node_full_key_by_index(n)
+                                .unwrap()
+                                .collect::<Vec<_>>()
+                                .starts_with(&req.key_as_vec())
+                        }) {
+                            next_node = None;
+                        }
+                        calculator = req.inject(
+                            next_node.map(|n| trie_before_diff.node_full_key_by_index(n).unwrap()),
+                        );
+                    }
+                    InProgress::MerkleValue(req) => calculator = req.resume_unknown(), // TODO: maybe test this as well?
+                    InProgress::StorageValue(req) => {
+                        let value = if let trie::trie_structure::Entry::Occupied(
+                            trie::trie_structure::NodeAccess::Storage(e),
+                        ) = trie_before_diff.node(req.key_as_vec().into_iter())
+                        {
+                            Some(e.into_user_data().0.as_ref().unwrap().clone())
+                        } else {
+                            None
+                        };
+
+                        calculator =
+                            req.inject_value(value.as_deref().map(|v| (v, TrieEntryVersion::V0)));
+                    }
+                }
+            }
         };
 
-        // Create multiple tries, each with a different order of insertion for the nodes.
-        let mut tries = Vec::new();
-        for _ in 0..16 {
-            #[derive(Debug, Copy, Clone)]
-            enum Op {
-                Insert,
-                Remove,
-                ClearPrefix,
-            }
+        // Calculate the root of `trie_after_diff` separately through the `TrieStructure`.
+        let expected_hash = {
+            for node_index in trie_after_diff
+                .iter_ordered()
+                .collect::<Vec<_>>()
+                .into_iter()
+                .rev()
+            {
+                let mut node_access = trie_after_diff.node_by_index(node_index).unwrap();
 
-            let mut operations = final_storage
-                .iter()
-                .map(|k| (k.clone(), Op::Insert))
-                .collect::<Vec<_>>();
-            operations.shuffle(&mut rand::thread_rng());
+                let children = core::array::from_fn::<_, 16, _>(|n| {
+                    node_access
+                        .child(trie::Nibble::try_from(u8::try_from(n).unwrap()).unwrap())
+                        .map(|mut child| child.user_data().1.as_ref().unwrap().clone())
+                });
 
-            // Insert in `operations` a tuple of an insertion and removal.
-            for _ in 0..uniform_sample(0, 24) {
-                let mut base_key = match operations.choose(&mut rand::thread_rng()) {
-                    Some(op) => op.0.clone(),
-                    None => continue,
+                let is_root_node = node_access.is_root_node();
+                let partial_key = node_access.partial_key().collect::<Vec<_>>().into_iter();
+
+                let storage_value = match node_access.user_data().0.as_ref() {
+                    Some(v) => trie::trie_node::StorageValue::Unhashed(&v[..]),
+                    None => trie::trie_node::StorageValue::None,
                 };
 
-                for _ in 0..uniform_sample(0, 2) {
-                    base_key.push(Nibble::try_from(uniform_sample(0, 15)).unwrap());
-                }
-
-                let max_remove_index = operations
-                    .iter()
-                    .position(|(k, _)| *k == base_key)
-                    .unwrap_or(operations.len());
-
-                let remove_index =
-                    Uniform::new_inclusive(0, max_remove_index).sample(&mut rand::thread_rng());
-                let insert_index =
-                    Uniform::new_inclusive(0, remove_index).sample(&mut rand::thread_rng());
-                operations.insert(remove_index, (base_key.clone(), Op::Remove));
-                operations.insert(insert_index, (base_key, Op::Insert));
-            }
-
-            // Insert in `operations` a tuple of multiple insertions of the same prefix, and
-            // removal of said prefix.
-            for _ in 0..uniform_sample(0, 4) {
-                let mut base_key = match operations.choose(&mut rand::thread_rng()) {
-                    Some(op) => op.0.clone(),
-                    None => continue,
-                };
-
-                for _ in 0..uniform_sample(0, 2) {
-                    base_key.push(Nibble::try_from(uniform_sample(0, 15)).unwrap());
-                }
-
-                let max_remove_index = operations
-                    .iter()
-                    .position(|(k, _)| k.starts_with(&base_key))
-                    .unwrap_or(operations.len());
-
-                let mut remove_index =
-                    Uniform::new_inclusive(0, max_remove_index).sample(&mut rand::thread_rng());
-                operations.insert(remove_index, (base_key.clone(), Op::ClearPrefix));
-
-                for _ in 0..uniform_sample(0, 12) {
-                    let mut base_key = base_key.clone();
-                    for _ in 0..uniform_sample(0, 8) {
-                        base_key.push(Nibble::try_from(uniform_sample(0, 15)).unwrap());
-                    }
-
-                    if operations
-                        .iter()
-                        .take(remove_index)
-                        .any(|(k, _)| *k == base_key)
-                    {
-                        continue;
-                    }
-
-                    let insert_index =
-                        Uniform::new_inclusive(0, remove_index).sample(&mut rand::thread_rng());
-                    operations.insert(insert_index, (base_key, Op::Insert));
-                    remove_index += 1;
-                }
-            }
-
-            // Create a trie and applies `operations` on it.
-            let mut trie = trie::trie_structure::TrieStructure::new();
-            for (op_index, (key, op)) in operations.clone().into_iter().enumerate() {
-                match op {
-                    Op::Insert => match trie.node(key.into_iter()) {
-                        super::Entry::Vacant(e) => {
-                            e.insert_storage_value().insert((), ());
-                        }
-                        super::Entry::Occupied(super::NodeAccess::Branch(e)) => {
-                            e.insert_storage_value();
-                        }
-                        super::Entry::Occupied(super::NodeAccess::Storage(_)) => {
-                            unreachable!("index: {}\nops:{:?}", op_index, operations)
-                        }
+                let merkle_value = trie::trie_node::calculate_merkle_value(
+                    trie::trie_node::Decoded {
+                        children,
+                        partial_key,
+                        storage_value,
                     },
-                    Op::Remove => match trie.node(key.into_iter()) {
-                        super::Entry::Occupied(super::NodeAccess::Storage(e)) => {
-                            e.remove();
-                        }
-                        super::Entry::Vacant(_) => {
-                            unreachable!("index: {}\nops:{:?}", op_index, operations)
-                        }
-                        super::Entry::Occupied(super::NodeAccess::Branch(_)) => {
-                            unreachable!("index: {}\nops:{:?}", op_index, operations)
-                        }
-                    },
-                    Op::ClearPrefix => {
-                        trie.remove_prefix(key.into_iter());
-                    }
-                }
+                    is_root_node,
+                )
+                .unwrap();
+
+                node_access.into_user_data().1 = Some(merkle_value);
             }
 
-            tries.push(trie);
-        }
+            trie_after_diff
+                .root_user_data()
+                .map(|n| *<&[u8; 32]>::try_from(n.1.as_ref().unwrap().as_ref()).unwrap())
+                .unwrap_or(trie::empty_trie_merkle_value())
+        };
 
-        // Compare them to make sure they're equal.
-        for trie in 1..tries.len() {
-            tries[0].structure_equal(&tries[trie]);
+        // Actual test is here.
+        if obtained_hash != expected_hash {
+            panic!(
+                "\nexpected = {:?}\ncalculated = {:?}\ntrie_before = {:?}\ndiff = {:?}",
+                expected_hash, obtained_hash, trie_before_diff, diff
+            );
         }
     }
-}*/
+}
