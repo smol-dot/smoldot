fc6d8f4d7804dd85174e9fe4979f7bd6ab4a4072
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-07-27 12:56:26 +0200

    Remove TODO in noise.rs

--- lib/src/libp2p/connection/noise.rs
+++ lib/src/libp2p/connection/noise.rs
@@ -280,7 +280,6 @@ impl Noise {
         outer_read_write: &'a mut ReadWrite<TNow>,
     ) -> Result<InnerReadWrite<'a, TNow>, CipherError> {
         // Try to pull data from `outer_read_write` to decrypt it.
-        // TODO: https://github.com/smol-dot/smoldot/issues/944
         while self.rx_buffer_decrypted.is_empty()
             || self.inner_stream_expected_incoming_bytes > self.rx_buffer_decrypted.len()
         {
