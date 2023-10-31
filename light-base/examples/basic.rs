acc7ab06890f5b8c4ac5a766dc6e3b8c251a0d9b
Daan van der Plas <daanvdplas@live.nl>
2023-10-31 10:10:42 +0100

    Change to single example

--- light-base/examples/basic.rs
+++ light-base/examples/basic.rs
@@ -34,16 +34,13 @@ fn main() {
             env!("CARGO_PKG_VERSION").into(),
         ));
 
-    // Ask the client to connect to a chain.
-    let smoldot_light::AddChainSuccess {
-        chain_id,
-        json_rpc_responses,
-    } = client
+    // Ask the client to connect to Kusama.
+    let kusama_connection = client
         .add_chain(smoldot_light::AddChainConfig {
             // The most important field of the configuration is the chain specification. This is a
             // JSON document containing all the information necessary for the client to connect to said
             // chain.
-            specification: include_str!("../../demo-chain-specs/polkadot.json"),
+            specification: include_str!("../../demo-chain-specs/kusama.json"),
 
             // Configures some constants about the JSON-RPC endpoints.
             // It is also possible to pass `Disabled`, in which case the chain will not be able to
@@ -81,23 +78,47 @@ fn main() {
             user_data: (),
         })
         .unwrap();
+    // Connection to Kusama is now properly initialized.
 
-    // The chain is now properly initialized.
+    let mut kusama_json_rpc_responses = kusama_connection.json_rpc_responses.unwrap();
 
-    // `json_rpc_responses` can only be `None` if we had passed `json_rpc: Disabled` in the
-    // configuration.
-    let mut json_rpc_responses = json_rpc_responses.unwrap();
+    // Ask the client to connect to Statemine.
+    let statemine_connection = client
+        .add_chain(smoldot_light::AddChainConfig {
+            specification: include_str!("../../demo-chain-specs/kusama-statemine.json"),
+            json_rpc: smoldot_light::AddChainConfigJsonRpc::Enabled {
+                max_pending_requests: NonZeroU32::new(128).unwrap(),
+                max_subscriptions: 1024,
+            },
+            // Providing the allocated identifier for Kusama.
+            potential_relay_chains: vec![kusama_connection.chain_id].into_iter(),
+            database_content: "",
+            user_data: (),
+        })
+        .unwrap();
+    // Connection to Statemine is now properly initialized.
 
-    // Send a JSON-RPC request to the chain.
+    let mut statemine_json_rpc_responses = statemine_connection.json_rpc_responses.unwrap();
+
+    // Send a JSON-RPC request to a chain, Kusama & Statemine in this case.
     // The example here asks the client to send us notifications whenever the new best block has
     // changed.
     // Calling this function only queues the request. It is not processed immediately.
     // An `Err` is returned immediately if and only if the request isn't a proper JSON-RPC request
     // or if the channel of JSON-RPC responses is clogged.
+    //
+    // Kusama:
     client
         .json_rpc_request(
             r#"{"id":1,"jsonrpc":"2.0","method":"chain_subscribeNewHeads","params":[]}"#,
-            chain_id,
+            kusama_connection.chain_id,
+        )
+        .unwrap();
+    // Statemine:
+    client
+        .json_rpc_request(
+            r#"{"id":1,"jsonrpc":"2.0","method":"chain_subscribeNewHeads","params":[]}"#,
+            statemine_connection.chain_id,
         )
         .unwrap();
 
@@ -105,8 +126,14 @@ fn main() {
     // JSON-RPC responses.
     smol::block_on(async move {
         loop {
-            let response = json_rpc_responses.next().await.unwrap();
-            println!("JSON-RPC response: {response}");
+            // The block time of a relay chain is 6 seconds and of a parachain 12 seconds.
+            // As a result, this loop fetches Kusama responses twice for every Statemine response.
+            for _ in 0..2 {
+                let response = kusama_json_rpc_responses.next().await.unwrap();
+                println!("Kusama JSON-RPC response: {response}\n");
+            }
+            let response = statemine_json_rpc_responses.next().await.unwrap();
+            println!("Statemine JSON-RPC response: {response}\n");
         }
-    })
+    });
 }
