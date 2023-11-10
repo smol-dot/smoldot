bb04b9988f054233576845cf5e027fa34fe4cc11
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-11-10 11:26:50 +0100

    Docfix

--- lib/src/network/service.rs
+++ lib/src/network/service.rs
@@ -873,7 +873,8 @@ where
     /// This function guarantees that the [`ConnectionId`] always refers to a connection that
     /// is still alive, in the sense that [`SingleStreamConnectionTask::inject_coordinator_message`]
     /// or [`MultiStreamConnectionTask::inject_coordinator_message`] has never returned `None`
-    /// and that no [`Event::Shutdown`] has been generated for this connection.
+    /// and that no [`Event::Disconnected`] or [`Event::PreHandshakeDisconnected`] has been
+    /// generated for this connection.
     pub fn pull_message_to_connection(
         &mut self,
     ) -> Option<(ConnectionId, CoordinatorToConnection)> {
