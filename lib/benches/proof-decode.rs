44883bf2fc1bd61a9bf86867e33004f668d4f0fd
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-12-11 11:34:02 +0100

    Add benchmarks for various operations

--- lib/benches/proof-decode.rs
+++ lib/benches/proof-decode.rs
@@ -15,21 +15,35 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput};
+use rand::Rng as _;
 
 fn benchmark_proof_decode(c: &mut Criterion) {
     let mut group = c.benchmark_group("proof-decode");
 
     struct Proof<'a> {
         data: &'a [u8],
+        trie_root: [u8; 32],
     }
 
     let proofs: &[Proof] = &[
         Proof {
             data: &include_bytes!("./proof-decode-small")[..],
+            trie_root: hex::decode(
+                "29d0d972cd27cbc511e9589fcb7a4506d5eb6a9e8df205f00472e5ab354a4e17",
+            )
+            .unwrap()
+            .try_into()
+            .unwrap(),
         },
         Proof {
             data: &include_bytes!("./proof-decode-large")[..],
+            trie_root: hex::decode(
+                "886ca67c0e8d0003e2531bb25b1da0fe2f80d4b9eb6719e819f363265bd670fa",
+            )
+            .unwrap()
+            .try_into()
+            .unwrap(),
         },
     ];
 
@@ -46,6 +60,82 @@ fn benchmark_proof_decode(c: &mut Criterion) {
                     )
                     .unwrap()
                 })
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("get-storage-value", proof.data.len()),
+            &smoldot::trie::proof_decode::decode_and_verify_proof(
+                smoldot::trie::proof_decode::Config { proof: proof.data },
+            )
+            .unwrap(),
+            |b, i| {
+                b.iter_batched(
+                    || {
+                        (0..rand::thread_rng().gen_range(12..48))
+                            .map(|_| rand::random())
+                            .collect::<Vec<_>>()
+                    },
+                    |key| i.storage_value(&proof.trie_root, &key),
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("next-key", proof.data.len()),
+            &smoldot::trie::proof_decode::decode_and_verify_proof(
+                smoldot::trie::proof_decode::Config { proof: proof.data },
+            )
+            .unwrap(),
+            |b, i| {
+                b.iter_batched(
+                    || {
+                        (0..rand::thread_rng().gen_range(12..48))
+                            .map(|_| rand::thread_rng().gen_range(0..16).try_into().unwrap())
+                            .collect::<Vec<_>>()
+                    },
+                    |key| i.next_key(&proof.trie_root, &key, false, &[], true),
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("closest-descendant-merkle-value", proof.data.len()),
+            &smoldot::trie::proof_decode::decode_and_verify_proof(
+                smoldot::trie::proof_decode::Config { proof: proof.data },
+            )
+            .unwrap(),
+            |b, i| {
+                b.iter_batched(
+                    || {
+                        (0..rand::thread_rng().gen_range(12..48))
+                            .map(|_| rand::thread_rng().gen_range(0..16).try_into().unwrap())
+                            .collect::<Vec<_>>()
+                    },
+                    |key| i.closest_descendant_merkle_value(&proof.trie_root, &key),
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("closest-ancestor", proof.data.len()),
+            &smoldot::trie::proof_decode::decode_and_verify_proof(
+                smoldot::trie::proof_decode::Config { proof: proof.data },
+            )
+            .unwrap(),
+            |b, i| {
+                b.iter_batched(
+                    || {
+                        (0..rand::thread_rng().gen_range(12..48))
+                            .map(|_| rand::thread_rng().gen_range(0..16).try_into().unwrap())
+                            .collect::<Vec<_>>()
+                    },
+                    |key| i.closest_ancestor_in_proof(&proof.trie_root, &key),
+                    BatchSize::SmallInput,
+                )
             },
         );
     }
