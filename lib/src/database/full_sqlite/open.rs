fe5d938536ea340b3b92d04157d6a84a4848ac65
Pierre Krieger <pierre.krieger1708@gmail.com>
2024-01-04 16:09:27 +0100

    Remove all the now-useless stuff from the database

--- lib/src/database/full_sqlite/open.rs
+++ lib/src/database/full_sqlite/open.rs
@@ -116,31 +116,6 @@ Keys in that table:
 
  - `finalized` (number): Height of the finalized block, as a 64bits big endian number.
 
- - `grandpa_authorities_set_id` (number): Id of the authorities set that must finalize the block
- right after the finalized block. The value is 0 at the genesis block, and increased by 1 at every
- authorities change. Missing if and only if the chain doesn't use Grandpa.
-
- - `grandpa_scheduled_target` (number): Height of the block where the authorities found in
- `grandpa_scheduled_authorities` will be triggered. Blocks whose height is strictly higher than
- this value must be finalized using the new set of authorities. This authority change must have
- been scheduled in or before the finalized block. Missing if no change is scheduled or if the
- chain doesn't use Grandpa.
-
- - `aura_slot_duration` (number): Duration of an Aura slot in milliseconds. Missing if and only if
- the chain doesn't use Aura.
-
- - `babe_slots_per_epoch` (number): Number of slots per Babe epoch. Missing if and only if the
- chain doesn't use Babe.
-
- - `babe_finalized_epoch` (blob): SCALE encoding of a structure that contains the information
- about the Babe epoch used for the finalized block. Missing if and only if the finalized
- block is block #0 or the chain doesn't use Babe.
-
- - `babe_finalized_next_epoch` (blob): SCALE encoding of a structure that contains the information
- about the Babe epoch that follows the one described by `babe_finalized_epoch`. If the
- finalized block is block #0, then this contains information about epoch #0. Missing if and
- only if the chain doesn't use Babe.
-
 */
 CREATE TABLE meta(
     key STRING NOT NULL PRIMARY KEY,
@@ -220,37 +195,6 @@ CREATE TABLE blocks_body(
     FOREIGN KEY (hash) REFERENCES blocks(hash) ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE INDEX blocks_body_by_block ON blocks_body(hash);
-
-/*
-List of public keys and weights of the GrandPa authorities that must finalize the children of the
-finalized block. Empty if the chain doesn't use Grandpa.
-*/
-CREATE TABLE grandpa_triggered_authorities(
-    idx INTEGER NOT NULL PRIMARY KEY,
-    public_key BLOB NOT NULL,
-    weight INTEGER NOT NULL,
-    CHECK(length(public_key) == 32)
-);
-
-/*
-List of public keys and weights of the GrandPa authorities that will be triggered at the block
-found in `grandpa_scheduled_target` (see `meta`). Empty if the chain doesn't use Grandpa.
-*/
-CREATE TABLE grandpa_scheduled_authorities(
-    idx INTEGER NOT NULL PRIMARY KEY,
-    public_key BLOB NOT NULL,
-    weight INTEGER NOT NULL,
-    CHECK(length(public_key) == 32)
-);
-
-/*
-List of public keys of the Aura authorities that must author the children of the finalized block.
-*/
-CREATE TABLE aura_finalized_authorities(
-    idx INTEGER NOT NULL PRIMARY KEY,
-    public_key BLOB NOT NULL,
-    CHECK(length(public_key) == 32)
-);
 
 PRAGMA user_version = 1;
 
