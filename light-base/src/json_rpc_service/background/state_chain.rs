e43be2f447235dccbba32ee7c665b61e7c8cda7a
Pierre Krieger <pierre.krieger1708@gmail.com>
2024-01-10 14:52:59 +0100

    Make it compile again

--- light-base/src/json_rpc_service/background/state_chain.rs
+++ light-base/src/json_rpc_service/background/state_chain.rs
@@ -21,7 +21,7 @@ use super::{legacy_state_sub, Background, GetKeysPagedCacheKey, PlatformRef};
 
 use crate::{log, sync_service};
 
-use alloc::{format, string::ToString as _, sync::Arc, vec, vec::Vec};
+use alloc::{borrow::ToOwned as _, format, string::ToString as _, sync::Arc, vec, vec::Vec};
 use core::{iter, num::NonZeroU32, time::Duration};
 use futures_channel::oneshot;
 use smoldot::{
