2496cb34bf80a0727de74f79e035bc5e7c48da88
Pierre Krieger <pierre.krieger1708@gmail.com>
2024-06-17 10:01:55 +0200

    PR link and comment tweak

--- light-base/src/runtime_service.rs
+++ light-base/src/runtime_service.rs
@@ -896,7 +896,10 @@ async fn run_background<TPlat: PlatformRef>(
                     }
                 } else if any_subscription || any_pending_subscription {
                     // Only start subscribing to the sync service if there is any pending
-                    // runtime service subscription.
+                    // or active runtime service subscription.
+                    // Note that subscriptions to the runtime service aren't destroyed when the
+                    // sync service subscriptions is lost but when the sync service is
+                    // resubscribed.
                     WakeUpReason::MustSubscribe
                 } else {
                     future::pending().await
--- wasm-node/CHANGELOG.md
+++ wasm-node/CHANGELOG.md
@@ -4,7 +4,7 @@
 
 ### Fixed
 
-- Fix runtime service getting stuck if it was subscribed to before the GrandPa warp sync finishes, and that the runtime of the finalized block in the chain specification is available on the peer-to-peer network.
+- Fix runtime service getting stuck if it was subscribed to before the GrandPa warp sync finishes, and that the runtime of the finalized block in the chain specification is available on the peer-to-peer network. ([#1874](https://github.com/smol-dot/smoldot/pull/1874))
 
 ## 2.0.28 - 2024-05-30
 
