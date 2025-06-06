120738c5c4ba45cb3a0771ced779bc94ce6fb3d8
Pierre Krieger <pierre.krieger1708@gmail.com>
2025-06-06 15:03:03 +0200

    Fix JSON-RPC request parsing errors being ignored (#2138)
    * Fix JSON-RPC request parsing errors being ignored
    
    * PR link
    
    * Fix test
    
    * Fix whitespace
    
    * Fix behavior for notifications

--- light-base/src/json_rpc_service/background.rs
+++ light-base/src/json_rpc_service/background.rs
@@ -704,16 +704,28 @@ pub(super) async fn run<TPlat: PlatformRef>(
 
             WakeUpReason::IncomingJsonRpcRequest(request_json) => {
                 // New JSON-RPC request pulled from the channel.
-                let Ok((request_id_json, request_parsed)) =
-                    methods::parse_jsonrpc_client_to_server(&request_json)
-                else {
-                    // Request has failed to parse. Immediately return an answer.
-                    let _ = me
-                        .responses_tx
-                        .send(parse::build_parse_error_response())
-                        .await;
-                    continue;
-                };
+                let (request_id_json, request_parsed) =
+                    match methods::parse_jsonrpc_client_to_server(&request_json) {
+                        Ok(r) => r,
+                        Err(methods::ParseClientToServerError::JsonRpcParse(_)) => {
+                            // Request has failed to parse. Immediately return an answer.
+                            let _ = me
+                                .responses_tx
+                                .send(parse::build_parse_error_response())
+                                .await;
+                            continue;
+                        }
+                        Err(methods::ParseClientToServerError::Method { request_id, error }) => {
+                            // Invalid method or parameters. Immediately return an answer.
+                            let _ = me.responses_tx.send(error.to_json_error(request_id)).await;
+                            continue;
+                        }
+                        Err(methods::ParseClientToServerError::UnknownNotification { .. }) => {
+                            // Invalid notification-style request. As per spec, we simply
+                            // ignore them.
+                            continue;
+                        }
+                    };
 
                 // Print a warning for legacy JSON-RPC API functions.
                 match request_parsed {
